da429d3ce42ca711a09b700b12a35b303b1f818b
Winhack9702 <114675009+WinHack9702GitHub@users.noreply.github.com>
2022-10-05 20:10:03 +0300

    Add files via upload

---
+++ 0.1/Unziper.bat
@@ -0,0 +1,47 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCqDJOl4RJyA8Fvkk7VijLUK1QT8WDhLM1pbE6YhXeEwdp/a2b+yOPAG5F/tZdgk1XU6
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCqDJOl4RJyA8Fvkk7VijLUK1QT8WDhLM1pbE6YhY+sxQL/u1K6HMOUB1hWqJKoY2GBTiM4eQh5Ae3I=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title WinXPUnziper 0.1
+echo Welcome to the XPunziper 0.1!To unzip some files of image to Unzip0.0 click any key
+pause >> nul
+md Unzip0.1
+md Unzip0.1\dll
+md Unzip0.1\exe
+md Unzip0.1\bmp
+EXPAND.exe shell32.dl_ Unzip0.1\dll\shell32.dll
+EXPAND.exe logonui.ex_ Unzip0.1\exe\logonui.exe
+EXPAND.exe explorer.ex_ Unzip0.1\exe\explorer.exe
+EXPAND.exe SETUP_W.bm_ Unzip0.1\bmp\SETUP_W.bmp
+echo Done! Unziped to Unzip0.1 Click any key to exit
+pause >> nul
